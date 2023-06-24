c00790d69a9c4e6d22fbc072973a7ac1233b76e3
Luka Pršina <prsinaluka@gmail.com>
2023-06-24 13:13:58 +0200

    errors

---
+++ web/vite.config.ts
@@ -0,0 +1,10 @@
+import solid from "solid-start/vite";
+import { defineConfig } from "vite";
+  
+export default defineConfig(() => {
+  return {
+    plugins: [solid({ ssr: true })],
+    ssr: { external: ["@prisma/client"] },
+  };
+});
+  
