708296e39355976308080be281ba1bce78e41e7f
Luka Pršina <prsinaluka@gmail.com>
2023-07-04 11:10:06 +0200

    spring now mine

--- web/src/routes/spring.tsx
+++ web/src/routes/spring.tsx
@@ -2,39 +2,78 @@ import type { VoidComponent } from "solid-js";
 import { animated, config, createSprings } from "solid-spring";
 import { DragGesture } from "@use-gesture/vanilla";
 import { For, onMount } from "solid-js";
-import swap from "lodash-move";
 
-const clamp = (value: number, min: number, max: number) => Math.min(Math.max(value, min), max)
+function clamp(value: number, min: number, max: number) {
+    return Math.min(Math.max(value, min), max)
+}
 
-const fn = (
+function swap<T>(array: T[], moveIndex: number, toIndex: number) {
+    const item = array[moveIndex];
+    const length = array.length;
+    const diff = moveIndex - toIndex;
+
+    if (diff > 0) {
+        // move left
+        return [
+            ...array.slice(0, toIndex),
+            item,
+            ...array.slice(toIndex, moveIndex),
+            ...array.slice(moveIndex + 1, length)
+        ];
+    } else if (diff < 0) {
+        // move right
+        const targetIndex = toIndex + 1;
+        return [
+            ...array.slice(0, moveIndex),
+            ...array.slice(moveIndex + 1, targetIndex),
+            item,
+            ...array.slice(targetIndex, length)
+        ];
+    }
+    return array;
+}
+
+function getSpringsProps(
     order: number[],
     active = false,
     originalIndex = 0,
     curIndex = 0,
     y = 0
-) => (index: number) =>
-        active && index === originalIndex
-            ? {
+) {
+    const func = (index: number) => {
+        let props;
+
+        if (active && index === originalIndex) {
+            props = {
                 y: curIndex * 100 + y,
                 scale: 1.1,
                 zIndex: 1,
                 shadow: 15,
-                immediate: (key: string) => key === "zIndex",
+                immediate: (key: string) => key === "z-index",
                 config: (key: string) => (key === "y" ? config.stiff : config.default)
             }
-            : {
+        }
+        else {
+            props = {
                 y: order.indexOf(index) * 100,
                 scale: 1,
                 zIndex: 0,
                 shadow: 1,
                 immediate: false
-            };
+            }
+        };
+
+        return props;
+    }
+
+    return func;
+}
 
 const DraggableList = (props: { items: string[] }) => {
     let order = props.items.map((_, index) => index); // Store indicies as a local ref, this represents the item order
     const divs: HTMLDivElement[] = [];
 
-    const styles = createSprings(props.items.length, fn(order)); // Create springs, each corresponds to an item, controlling its transform, scale, etc.
+    const styles = createSprings(props.items.length, getSpringsProps(order)); // Create springs, each corresponds to an item, controlling its transform, scale, etc.
 
     onMount(() => {
         divs.forEach((div, originalIndex) => {
@@ -46,19 +85,22 @@ const DraggableList = (props: { items: string[] }) => {
                     props.items.length - 1
                 );
                 const newOrder = swap(order, curIndex, curRow);
-                styles.ref.start(fn(newOrder, active, originalIndex, curIndex, y)); // Feed springs new style data, they'll animate the view without causing a single render
+                styles.ref.start(getSpringsProps(newOrder, active, originalIndex, curIndex, y)); // Feed springs new style data, they'll animate the view without causing a single render
                 if (!active) order = newOrder;
             });
         });
     });
 
     return (
-        <div>
+        <div class="w-96" style={{
+            "height": (props.items.length * 100).toString() + "px",
+        }}>
             <For each={styles()}>{({ zIndex, shadow, y, scale }, i) => (
                 <animated.div
                     ref={(ref) => (divs[i()] = ref)}
+                    class="absolute w-96 h-20 origin-[50% 50% 0px] rounded pl-8 touch-none select-none bg-slate-500"
                     style={{
-                        "z-index": zIndex,
+                        "z-index": zIndex.to((z) => z.toString()),
                         "box-shadow": shadow.to(
                             (s) => `rgba(0, 0, 0, 0.15) 0px ${s}px ${2 * s}px 0px`
                         ),
@@ -74,7 +116,7 @@ const DraggableList = (props: { items: string[] }) => {
 
 const App: VoidComponent = () => {
     return (
-        <div>
+        <div class="flex justify-center items-center w-full h-screen">
             <DraggableList items={"Lorem ipsum dolor sit".split(" ")} />
         </div>
     )
