4e8dd03bab03544ff10dce7f8be0d5578e1bf5d9
Luka Pršina <luk.prsina@gmail.com>
2023-07-04 23:27:17 +0200

    array solved

--- web/src/routes/array.tsx
+++ web/src/routes/array.tsx
@@ -1,89 +1,82 @@
-import type { Owner } from "solid-js";
-import { useContext, type ParentComponent, type VoidComponent, createContext, createSignal, getOwner, createEffect } from "solid-js";
-import type { SetStoreFunction } from "solid-js/store";
-import { createStore } from "solid-js/store";
+import { useContext, type ParentComponent, type VoidComponent, createContext, createSignal } from "solid-js";
+import { createStore, produce } from "solid-js/store";
 
 type SomeType = {
     prop: number;
 }
 
-const Context = createContext<{
-    getAllData: () => { data: SomeType, id: Owner }[],
-    setAllData: SetStoreFunction<{
-        data: SomeType;
-        id: Owner;
-    }[]>
-    /* {
-        add: (id: Owner, data: SomeType) => void,
-        edit: (id: Owner, data: SomeType) => void,
-        delete: (id: Owner) => void
-    } */
-}>();
+const Context = createContext<[
+    { data: SomeType, id: number }[],
+    {
+        add: (id: number, data: SomeType) => void,
+        edit: (id: number, data: SomeType) => void,
+        delete: (id: number) => void
+    }
+
+]>();
 export function useParent() { return useContext(Context); }
 
 const Parent: ParentComponent = (props) => {
-    const [allData, setAllData] = createStore<{ data: SomeType, id: Owner }[]>([]);
+    // the store is dependant on children
+    // it doesn't update them
+    const [allData, setAllData] = createStore<{ data: SomeType, id: number }[]>([]);
 
-    createEffect(() => {
-        console.info("parent")
-        for (const data of allData) {
-            console.log(data.data.prop)
-        }
-        console.info("parent done")
-    })
+    // this works if you need context
+    // otherwise an array is better, because you can
+    // add and remove
 
     return (
         <Context.Provider
-            value={{
-                getAllData() { return allData },
-                setAllData
-                /* {
-                    add(id: Owner, data: SomeType) {
+            value={[
+                allData,
+                {
+                    add(id: number, data: SomeType) {
                         setAllData([...allData, { id, data }]);
                     },
-                    edit(id: Owner, data: SomeType) {
+                    edit(id: number, data: SomeType) {
                         setAllData(todo => todo.id == id, "data", data)
                     },
-                    delete(id: Owner) {
+                    delete(id: number) {
                         setAllData(produce((data) => {
-                            const index = data.findIndex(item => item.id === id);
+                            const index = data.findIndex(item => item.id == id)
                             if (index !== -1) {
                                 data.splice(index, 1);
                             }
                         }));
                     }
-                } */
-            }}
+                }
+            ]}
         >
             {props.children}
+            <button
+                onClick={() => {
+                    console.info("parent")
+                    for (const data of allData) {
+                        console.log(data, data.data.prop)
+                    }
+                    console.info("parent done")
+                }}
+            >See store</button>
         </Context.Provider >
     )
 }
 
+let sequential = 0;
 const Child: VoidComponent = () => {
     const [data, setData] = createSignal<SomeType>()
 
-    const owner = getOwner();
-    if (!owner) throw new Error("No owner");
+    /* const owner = getOwner();
+    if (!owner) throw new Error("No owner"); */
 
     const parent_context = useParent();
     if (!parent_context) throw new Error("No context");
 
-    const { getAllData, setAllData } = parent_context;
-
-    const new_data = { prop: 0 };
-    setAllData([...getAllData(), { id: owner, data: new_data }]);
-
-    createEffect(() => {
-        // TODO: maybe more efficient by parent calling child directly
-        console.info("child")
-        for (const parent_data of getAllData()) {
-            if (parent_data.id == owner) {
-                setData(parent_data.data);
-            }
-        }
-        console.info("child done")
-    })
+    const [, funcs] = parent_context;
+    const original_index = sequential;
+    const new_data = { prop: original_index };
+    funcs.add(original_index, new_data);
+    setData(new_data)
+    sequential++;
 
     return (
         <div>
@@ -94,19 +87,23 @@ const Child: VoidComponent = () => {
                     if (!original) throw new Error("No data");
 
                     const new_prop = original.prop + 1;
-                    // parent_context[1].edit(owner, { prop: new_prop })
+                    const new_data = { prop: new_prop };
+
+                    console.log("Changing data", data(), new_data)
+                    setData(new_data)
+                    funcs.edit(original_index, new_data)
                 }}
             >
                 Edit
             </button>{' '}
             <button
                 onClick={() => {
-                    // parent_context[1].delete(owner)
+                    funcs.delete(original_index)
                 }}
             >
                 Delete
             </button>
-        </div>
+        </div >
     )
 }
 
@@ -117,6 +114,7 @@ const App: VoidComponent = () => {
             <Child />
             <Child />
         </Parent>
+        <br />
         <button>Add child</button>
     </>
 }
