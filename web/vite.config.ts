002ab78cab036e55f518df4b25d1c9ec82a95367
Luka Pršina <luk.prsina@gmail.com>
2023-07-15 18:18:16 +0200

    moving to vercel

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,12 +2,13 @@ import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import copy from 'rollup-plugin-copy'
 import suid from "@suid/vite-plugin";
+import vercel from "solid-start-vercel";
 
 export default defineConfig(() => {
   return {
     plugins: [
       suid(),
-      solid({ ssr: true }),
+      solid({ ssr: true, adapter: vercel({ edge: false }) }),
       copy({
         targets: [
           {
